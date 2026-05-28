071ad24bf84fbf64c926ede638f8502e16007c58
Priyawarat <priyearya223@gmail.com>
2026-05-29 01:56:56 +0530

    completed comment

--- src/main/java/com/infy/practice/PracticeApplication.java
+++ src/main/java/com/infy/practice/PracticeApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//Homework completed
 @SpringBootApplication
 public class PracticeApplication implements CommandLineRunner {
 
